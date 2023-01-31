361f5ea4d5d7f7b83d93deac3d09bfe22dd4194f
medicago <medicago087@gmail.com>
2023-01-31 20:29:17 +0800

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, sleep } from 'koishi'
 import {} from '@koishijs/plugin-adapter-onebot'
+import { randomInt } from 'crypto'
 
 export const name = 'qqgroup-manager'
+
+export const usage = `
+## 注意事项
++ 本插件只使用权限等级作为判断依据，群主和管理员并不天然具有操作权限。
++ 请使用 \`authorize <value> -u <user>\` 为他人加权。若bot主人无权限，请修改数据库user表中authority字段为你想要的权限等级。
++ 注：vscqq等oicq用户暂不能使用回复消息相关功能。
+`
 
 export interface Config {
   setGroupAdmin: number
@@ -13,6 +21,11 @@ export interface Config {
   setGroupCard: number
   setSelfSpecialTitle: number
   setGroupSpecialTitle: number
+  deleteMsg: number
+  deleteMsgNotice: boolean
+  setEssenceMsg: number
+  setEssenceMsgCommand: string
+  deleteEssenceMsgCommand: string
 }
 
 export const Config: Schema<Config> = Schema.object({
@@ -25,13 +38,17 @@ export const Config: Schema<Config> = Schema.object({
   setGroupCard: Schema.number().default(4).description('可修改群名片的权限等级。'),
   setSelfSpecialTitle: Schema.number().default(1).description('可申请头衔的权限等级。'),
   setGroupSpecialTitle: Schema.number().default(4).description('可修改头衔的权限等级。'),
+  deleteMsg: Schema.number().default(4).description('可撤回消息的权限等级。'),
+  deleteMsgNotice: Schema.boolean().default(true).description('撤回消息后是否显示提示语'),
+  setEssenceMsg: Schema.number().default(4).description('可设置精华消息的权限等级。'),
+  setEssenceMsgCommand: Schema.string().default('设置精华消息').description('设置精华消息的指令。'),
+  deleteEssenceMsgCommand: Schema.string().default('取消精华消息').description('取消精华消息的指令。'),
 }).description('权限设置')
 
 export function apply(ctx: Context, config: Config) {
 
   // 升为管理
   ctx.command('manager','群管工具组').subcommand('升为管理 <qq:user>', '如：取消管理 @user(前需要空格)', {authority: config.setGroupAdmin, checkArgCount: true})
-    .usage('样例：升为管理 @user(前需要空格)')
     .action(async ({session}, qq)=>{
       const [platform, qqnum] = qq.split(':')
       session.onebot.setGroupAdmin(
@@ -48,7 +65,7 @@ export function apply(ctx: Context, config: Config) {
     })
 
   // 取消管理
-  ctx.command('manager','群管工具组').subcommand('取消管理 <qq:user>', '如：取消管理 @user(前需要空格)', {authority: config.setGroupAdmin, checkArgCount: true})
+  ctx.command('manager').subcommand('取消管理 <qq:user>', '如：取消管理 @user(前需要空格)', {authority: config.setGroupAdmin, checkArgCount: true})
     .action(async ({session}, qq)=>{
       const [platform, qqnum] = qq.split(':')
       session.onebot.setGroupAdmin(
@@ -65,7 +82,7 @@ export function apply(ctx: Context, config: Config) {
     })
   
   // 踢出群聊
-  ctx.command('manager','群管工具组').subcommand('踢出群聊 <qq:user>','如：踢出群聊 @user(前需要空格)', {authority: config.setGroupKick, checkArgCount: true})
+  ctx.command('manager').subcommand('踢出群聊 <qq:user>','如：踢出群聊 @user(前需要空格)', {authority: config.setGroupKick, checkArgCount: true})
     .action(async ({session}, qq)=>{
       const [platform, qqnum] = qq.split(':')
       const {nickname} = await session.onebot.getGroupMemberInfo(
@@ -82,7 +99,7 @@ export function apply(ctx: Context, config: Config) {
     })
 
   // 开启全员禁言
-  ctx.command('manager','群管工具组').subcommand('开启全员禁言', {authority: config.setGroupWholeBan, checkArgCount: true})
+  ctx.command('manager').subcommand('开启全员禁言', {authority: config.setGroupWholeBan})
     .action(({session})=>{
       session.onebot.setGroupWholeBan(
 				session.guildId,
@@ -92,7 +109,7 @@ export function apply(ctx: Context, config: Config) {
     })
 
   // 关闭全员禁言
-  ctx.command('manager','群管工具组').subcommand('解除全员禁言', {authority: config.setGroupWholeBan, checkArgCount: true})
+  ctx.command('manager').subcommand('解除全员禁言', {authority: config.setGroupWholeBan})
     .action(({session})=>{
       session.onebot.setGroupWholeBan(
 				session.guildId,
@@ -102,7 +119,7 @@ export function apply(ctx: Context, config: Config) {
     })
 
   // 禁言
-  ctx.command('manager','群管工具组').subcommand('禁言 <qq:user> <duration:number> <type>','如：禁言 @user(前后需要空格) 1 分钟',{authority: config.setGroupBan, checkArgCount: true})
+  ctx.command('manager').subcommand('禁言 <qq:user> <duration:number> <type>','如：禁言 @user(前后需要空格) 1 分钟',{authority: config.setGroupBan, checkArgCount: true})
     .action(({session}, qq, duration, type)=>{
       const [platform, qqnum] = qq.split(':')
       switch(type){
@@ -131,7 +148,7 @@ export function apply(ctx: Context, config: Config) {
     })
 
   // 解除禁言
-  ctx.command('manager','群管工具组').subcommand('解除禁言 <qq:user>','如：解除禁言 @user(前需要空格)',{authority: config.setGroupBan, checkArgCount: true})
+  ctx.command('manager').subcommand('解除禁言 <qq:user>','如：解除禁言 @user(前需要空格)',{authority: config.setGroupBan, checkArgCount: true})
     .action(({session}, qq)=>{
       const [platform, qqnum] = qq.split(':')
       session.onebot.setGroupBan(
@@ -174,7 +191,7 @@ export function apply(ctx: Context, config: Config) {
   ctx.command('manager').subcommand('修改名片 <qq:user> <text:string>','如：修改名片 @user(前后需要空格) 恋恋',{authority: config.setGroupCard, checkArgCount: true})
     .action(({session}, qq, text)=>{
       const [platform, qqnum] = qq.split(':')
-      if(text.length > 60){ // 长度限制60，1汉字=3，在做了
+      if(new TextEncoder().encode(text).length > 60){ // 长度限制60
 				return '名字太长啦！';
 			}
       session.onebot.setGroupCard(
@@ -185,10 +202,10 @@ export function apply(ctx: Context, config: Config) {
       return '嗯！已经改好啦~';
     })
   
-  // 申请头衔
+  // 申请头衔 
   ctx.command('manager').subcommand('申请头衔 <text:string>','如：申请头衔 恋恋',{authority: config.setSelfSpecialTitle, checkArgCount: true})
     .action(({session},text)=>{
-      if(text.length > 18){ // 长度限制18，1汉字=3，在做了
+      if(new TextEncoder().encode(text).length > 18){ // 长度限制18
 				return '头衔太长啦！';
 			}
       session.onebot.setGroupSpecialTitle(
@@ -196,14 +213,14 @@ export function apply(ctx: Context, config: Config) {
 				session.userId,
 			  text,
 			)
-      return '嗯！不错的头衔呢~';
+      return '已经改好啦~';
     })
 
   // 修改头衔
   ctx.command('manager').subcommand('修改头衔 <qq:user> <text:string>','如：修改头衔 @user(前后需要空格) 恋恋',{authority: config.setGroupSpecialTitle, checkArgCount: true})
     .action(({session}, qq, text)=>{
       const [platform, qqnum] = qq.split(':')
-      if(text.length > 18){ // 长度限制18，1汉字=3，在做了
+      if(new TextEncoder().encode(text).length > 18){ // 长度限制18
 				return '头衔太长啦！';
 			}
       session.onebot.setGroupSpecialTitle(
@@ -211,6 +228,42 @@ export function apply(ctx: Context, config: Config) {
 				qqnum,
 			  text,
 			)
-      return '嗯！已经改好啦~';
+      return '已经改好啦~';
     })
+
+  // 撤回
+  ctx.command('manager').subcommand('撤回', '回复一条消息 撤回', {authority: config.deleteMsg})
+  .action(async ({session})=>{
+    await session.onebot.deleteMsg(
+      session.quote.messageId,
+    )
+    await sleep(randomInt(500,1000))
+    await session.onebot.deleteMsg(
+      session.messageId,
+    )
+    console.log(config.deleteMsgNotice)
+    if(config.deleteMsgNotice === false){
+      return;
+    } else {
+      return '已经撤回啦~';
+    }
+  })
+
+  // 设精
+  ctx.command('manager').subcommand(config.setEssenceMsgCommand, '回复一条消息 设置精华消息', {authority: config.setEssenceMsg})
+  .action(({session})=>{
+    session.onebot.setEssenceMsg(
+      session.quote.messageId,
+    )
+    return '设置成功啦~';
+  })
+
+  // 取精
+  ctx.command('manager').subcommand(config.deleteEssenceMsgCommand, '回复一条消息 取消精华消息', {authority: config.setEssenceMsg})
+  .action(({session})=>{
+    session.onebot.deleteEssenceMsg(
+      session.quote.messageId,
+    )
+    return '取消成功啦~';
+  })
 }
